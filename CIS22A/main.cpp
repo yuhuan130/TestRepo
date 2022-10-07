5b5645b455a4e99ad7c9836594d1a0dcc306f542
Yuhuan <alexchien130@gmail.com>
2022-10-07 14:02:08 -0700

    Initial Commit

---
+++ CIS22A/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CIS22A
+//
+//  Created by 簡佑寰 on 2022/10/7.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
